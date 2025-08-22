625874afd20a25e4ef77e3bec23038b51e8d02f4
Hassan-HassanAbdullah <iqasir575@gmail.com>
2025-08-23 01:47:51 +0500

    Updated start script for Railway deployment

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/recipes', Routes); // Assuming you want to use the same routes for
 
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_DB_URI).then(() => {
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Connected to MongoDB');
     app.listen(process.env.PORT, () => {
         console.log('Server is running on http://localhost:'+ process.env.PORT);
